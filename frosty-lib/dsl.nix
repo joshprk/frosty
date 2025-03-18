b4ec0f3834e08b83b385e072ddb1ae7b7154a5d2
Joshua Park <joshuprk@gmail.com>
2025-03-18 17:04:16 -0400

    chore: add documentation for fn

--- frosty-lib/dsl.nix
+++ frosty-lib/dsl.nix
@@ -1,4 +1,36 @@
 {lib, ...}: {
+  /**
+  Creates a function f: X -> Y where the types of X and Y can be specified and
+  checked for type errors before execution.
+
+  # Example
+
+  ```nix
+  addThree = frosty.fn (frosty.types.int) (frosty.types.int)
+    (n: n + 3)
+  =>
+  addThree 3
+  =>
+  6
+  ```
+
+  # Type
+
+  ```
+  fn :: Unknown -> Unknown -> lambda
+
+  # Arguments
+
+  argType
+  : A single type or an attrset of input argument types.
+
+  returnType
+  : A single type which is the expected return type of the lambda
+
+  lambda
+  : The function to wrap with type-checking
+  ```
+  */
   fn = argType: returnType: lambda: arg: let
     checkArg = value:
       if (builtins.typeOf arg) == "set"
