48ff69eb8961b9ccc5eb6099069c1c02a12bda27
Joshua Park <joshuprk@gmail.com>
2025-03-18 08:08:08 -0400

    feat: remove fnOld

--- frosty-lib/dsl.nix
+++ frosty-lib/dsl.nix
@@ -25,24 +25,4 @@
         msg = "return value is not of type ${returnType.name}";
       in lib.throwIfNot cond msg value;
     in checkReturn (lambda (checkArg arg));
-
-  fnOld = argType: returnType: lambda:
-    args: let
-      checkArgs =
-        if (builtins.typeOf args) == "set" then
-          let
-            checkFn = name: value: let
-              resolved = if args ? name then value else argType.${name}.default;
-              cond = argType.${name}.check resolved;
-              msg = "";
-            in lib.throwIfNot cond msg resolved;
-          in builtins.mapAttrs checkFn args
-        else
-          let
-            resolved = if args != null then args else argType.default;
-            cond = argType.check resolved;
-            msg = "${toString args} is not of type ${argType.name}";
-          in lib.throwIfNot cond msg resolved;
-    in
-      lambda (checkArgs args);
 }
